1288734e3330655135365dd371dd2d9fa8826656
nigelb <nigel.blair@gmail.com>
2013-05-24 17:24:41 +1000

    Added data_files to setup call

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+import glob
 
 requires=[
     "requests",
@@ -37,4 +38,7 @@ setup(name='SerialGrabber',
             "serial_grabber = serial_grabber.SerialGrabber:main",
         ]
     },
+    data_files=[
+        ("/etc/SerialGrabber", glob.glob("example_config/*"))
+    ]
 )
