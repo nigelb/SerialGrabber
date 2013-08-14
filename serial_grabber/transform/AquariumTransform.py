cfed8c2c3776ce5f64352599c20e789441ff44a5
nigelb <nigel.blair@gmail.com>
2013-08-14 11:33:41 +1000

    Flatten out dict of transformed data

--- serial_grabber/transform/AquariumTransform.py
+++ serial_grabber/transform/AquariumTransform.py
@@ -21,22 +21,22 @@ from serial_grabber.util import config_helper
 
 def __temp(args):
     name, temp, tempSetPoint = args
-    return "Temperature", {"temp": temp, "set_point": tempSetPoint}
+    return "Temperature", {"Temperature_temp": temp, "Temperature_set_point": tempSetPoint}
 
 
 def __ph(args):
     name, ph, phSetPoint = args
-    return "PH", {"ph": ph, "set_point": phSetPoint}
+    return "PH", {"PH_ph": ph, "PH_set_point": phSetPoint}
 
 
 def __tempPID(args):
     name, kp, ki, kd, pidVal = args
-    return "TemperaturePID", {"Kp": kp, "Ki": ki, "Kd": kd, "pid_val": pidVal}
+    return "TemperaturePID", {"TemperaturePID_Kp": kp, "TemperaturePID_Ki": ki, "TemperaturePID_Kd": kd, "TemperaturePID_pid_val": pidVal}
 
 
 def __phPID(args):
     name, kp, ki, kd, pidVal = args
-    return "PHPID", {"Kp": kp, "Ki": ki, "Kd": kd, "pid_val": pidVal}
+    return "PHPID", {"PHPID_Kp": kp, "PHPID_Ki": ki, "PHPID_Kd": kd, "PHPID_pid_val": pidVal}
 
 row_map = {
     "temp": __temp,
@@ -61,7 +61,9 @@ class AquariumTransform(Transform):
             val = i.split(",")
             if val[0] in row_map.keys():
                 id, dat = row_map[val[0]](val)
-                result[id] = dat
+                for j in dat.keys():
+                    result[j] = dat[j]
+#                result[id] = dat
         result['ts'] = process_entry.data.time
         transform_result.data.payload = result
         transform_result.data.original_payload = original_payload
@@ -70,46 +72,51 @@ class AquariumTransform(Transform):
 
 def averageAquariumData(data):
     result = config_helper({
-        'PHPID': {"Kp": 0, "Ki": 0, "Kd": 0, "pid_val": 0},
-        "TemperaturePID": {"Kp": 0, "Ki": 0, "Kd": 0, "pid_val": 0},
-        "PH": {"ph": 0, "set_point": 0},
-        "Temperature": {"temp": 0, "set_point": 0}
+#        'PHPID': {"Kp": 0, "Ki": 0, "Kd": 0, "pid_val": 0},
+#        "TemperaturePID": {"Kp": 0, "Ki": 0, "Kd": 0, "pid_val": 0},
+#        "PH": {"ph": 0, "set_point": 0},
+#        "Temperature": {"temp": 0, "set_point": 0}
+        "PHPID_Kp": 0, "PHPID_Ki": 0, "PHPID_Kd": 0, "PHPID_pid_val": 0,
+        "TemperaturePID_Kp": 0, "TemperaturePID_Ki": 0, "TemperaturePID_Kd": 0, "TemperaturePID_pid_val": 0,
+        "PH_ph": 0, "PH_set_point": 0,
+        "Temperature_temp": 0, "Temperature_set_point": 0
     })
     _time = 0
     for element in data:
         _time += element.data.time
-        result.PHPID.Kp += float(element.data.payload.PHPID.Kp)
-        result.PHPID.Ki += float(element.data.payload.PHPID.Ki)
-        result.PHPID.Kd += float(element.data.payload.PHPID.Kd)
+        result.PHPID_Kp += float(element.data.payload.PHPID_Kp)
+        result.PHPID_Ki += float(element.data.payload.PHPID_Ki)
+        result.PHPID_Kd += float(element.data.payload.PHPID_Kd)
 
-        result.TemperaturePID.Kp += float(element.data.payload.TemperaturePID.Kp)
-        result.TemperaturePID.Ki += float(element.data.payload.TemperaturePID.Ki)
-        result.TemperaturePID.Kd += float(element.data.payload.TemperaturePID.Kd)
+        result.TemperaturePID_Kp += float(element.data.payload.TemperaturePID_Kp)
+        result.TemperaturePID_Ki += float(element.data.payload.TemperaturePID_Ki)
+        result.TemperaturePID_Kd += float(element.data.payload.TemperaturePID_Kd)
 
 
-        result.PH.ph += float(element.data.payload.PH.ph)
-        result.PH.set_point += float(element.data.payload.PH.set_point)
+        result.PH_ph += float(element.data.payload.PH_ph)
+        result.PH_set_point += float(element.data.payload.PH_set_point)
 
-        result.Temperature.temp += float(element.data.payload.Temperature.temp)
-        result.Temperature.set_point += float(element.data.payload.Temperature.set_point)
+        result.Temperature_temp += float(element.data.payload.Temperature_temp)
+        result.Temperature_set_point += float(element.data.payload.Temperature_set_point)
 
 
     length = len(data)
-    result.PHPID.Kp /= length
-    result.PHPID.Ki /= length
-    result.PHPID.Kd /= length
+    result.PHPID_Kp /= length
+    result.PHPID_Ki /= length
+    result.PHPID_Kd /= length
 
-    result.TemperaturePID.Kp /= length
-    result.TemperaturePID.Ki /= length
-    result.TemperaturePID.Kd /= length
+    result.TemperaturePID_Kp /= length
+    result.TemperaturePID_Ki /= length
+    result.TemperaturePID_Kd /= length
 
-    result.PH.ph /= length
-    result.PH.set_point /= length
+    result.PH_ph /= length
+    result.PH_set_point /= length
 
-    result.Temperature.temp /= length
-    result.Temperature.set_point /= length
-    toRet = {'data':{'payload':result.config_delegate, 'time':int(_time/length)}}
-    return toRet
+    result.Temperature_temp /= length
+    result.Temperature_set_point /= length
+    result.timestamp = int(_time/length)
+    toRet = {'data':{'payload':result.config_delegate, 'time':result.timestamp}}
+    return config_helper(toRet)
 
 
 
