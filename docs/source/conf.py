75df2019c8597399858a2a372e55409928011a7c
nigelb <nigel.blair@gmail.com>
2014-07-17 18:35:51 +1000

    Add mock modules

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,32 @@ import sys, os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath('../../example_config'))
+#sys.path.insert(0, os.path.abspath('../../example_config'))
+
+class Mock(object):
+
+    __all__ = []
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+MOCK_MODULES = ['pygtk', 'serial', 'SerialGrabber_Settings', 'SerialGrabber_State']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
 
 # -- General configuration -----------------------------------------------------
 
