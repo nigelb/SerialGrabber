3112cd2c527edd0cfc3ab8e415fe47fbe0e0f258
nigelb <nigel.blair@gmail.com>
2014-07-31 12:12:33 +1000

    Add missing import

--- serial_grabber/reader/Xbee.py
+++ serial_grabber/reader/Xbee.py
@@ -18,6 +18,7 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 import serial
+import SerialGrabber_Settings
 import time
 from serial_grabber import cache
 from serial_grabber.reader.SerialReader import SerialReader
