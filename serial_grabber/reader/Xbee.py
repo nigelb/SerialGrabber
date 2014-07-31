9c3fa89a7d326c6a79332366f71fa9823d742751
nigelb <nigel.blair@gmail.com>
2014-08-01 09:54:11 +1000

    Cleaned up code and added docstring

--- serial_grabber/reader/Xbee.py
+++ serial_grabber/reader/Xbee.py
@@ -16,8 +16,6 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-import base64
-import pickle
 
 import serial
 import SerialGrabber_Settings
@@ -28,83 +26,30 @@ from serial_grabber.util import config_helper, get_millis
 from serial import SerialException
 from xbee import ZigBee
 
-
-# packet_start_byte = 0x7E
-# escape_marker = 0x7D
-#
-#
-# class XbeeAPIm2Reader(SerialReader):
-#
-#     def run(self):
-#         config = config_helper({})
-#         config.counter = self.counter
-#         start = get_millis()
-#         dat = []
-#         while self.isRunning.running:
-#             try:
-#                 if self.stream is None:
-#                     self.setup()
-#                     start = get_millis()
-#                     continue
-#                 read_data = ""
-#                 current = None
-#
-#
-#                 #"if 0x7E is received at any time it can be assumed that a new packet has started regardless of length"
-#                 # http://www.digi.com/support/kbase/kbaseresultdetl?id=2199
-#
-#                 while self.isRunning.running and ord(current) != packet_start_byte:
-#                     current = self.read()
-#                     dat.append(current)
-#                 read_data = "".join(dat[:-1])
-#
-#                 if current is not None:
-#                     dat = [current]
-#
-#                 if (get_millis() - start)  <= SerialGrabber_Settings.startup_ignore_threshold_milliseconds:
-#                     self.logger.warn("Dropping data received inside startup threshold.")
-#                     continue
-#
-#                 if not ord(dat[0]) != packet_start_byte:
-#                     self.logger.error("There was unmatched input: %s"%read_data)
-#                 else:
-#                     cache.cache(cache.make_payload(format_data(read_data)))
-#                     config.counter.read()
-#                     config.counter.update()
-#
-#             except SerialException, se:
-#                 self.close()
-#                 return
-#             except Exception, e:
-#                 self.counter.error()
-#                 import traceback
-#                 traceback.print_exc()
-#             if self.stream is None: time.sleep(SerialGrabber_Settings.reader_error_sleep)
-#
-#     def read(self):
-#         """
-#         Reads a XBEE API Mode 2 stream and un-escapes it.
-#         """
-#         data = self.stream.read()
-#         if ord(data) == escape_marker:
-#             data = chr(0x20 ^ ord(self.stream.read()))
-#         return data
-
-# ZigBee.api_responses[b'\xa1'] = {'name': 'route_record_indicator',
-#                                  'structure': [
-#                                      {'name': 'source_addr_long', 'len': 8},
-#                                      {'name': 'source_addr', 'len': 2},
-#                                      {'name': 'rcv_options', 'len': 1},
-#                                      {'name': 'route_records_count', 'len': 1},
-#                                      {'name': 'route_records', 'len': None},
-#                                  ]}
-ZigBee.api_responses[b'\xa1'] = {'name': 'route_record_indicator', 'structure': [{'name': 'data', 'len': None}]}
-ZigBee.api_responses[b'\xa2'] = {'name': 'device_authenticated_indicator', 'structure': [{'name': 'data', 'len': None}]}
-ZigBee.api_responses[b'\xa3'] = {'name': 'many_to_one_route_request_indicator', 'structure': [{'name': 'data', 'len': None}]}
-ZigBee.api_responses[b'\xa4'] = {'name': 'register_joining_device_indicator', 'structure': [{'name': 'data', 'len': None}]}
-ZigBee.api_responses[b'\xa5'] = {'name': 'join_notification_status', 'structure': [{'name': 'data', 'len': None}]}
+#This if statement removes errors when building the documentation
+if 'api_responses' in ZigBee.__dict__:
+    ZigBee.api_responses[b'\xa1'] = {'name': 'route_record_indicator', 'structure': [{'name': 'data', 'len': None}]}
+    ZigBee.api_responses[b'\xa2'] = {'name': 'device_authenticated_indicator', 'structure': [{'name': 'data', 'len': None}]}
+    ZigBee.api_responses[b'\xa3'] = {'name': 'many_to_one_route_request_indicator', 'structure': [{'name': 'data', 'len': None}]}
+    ZigBee.api_responses[b'\xa4'] = {'name': 'register_joining_device_indicator', 'structure': [{'name': 'data', 'len': None}]}
+    ZigBee.api_responses[b'\xa5'] = {'name': 'join_notification_status', 'structure': [{'name': 'data', 'len': None}]}
 
 class DigiRadioReader(SerialReader):
+    """
+    Reads Digi Xbee/ZigBee API mode packets from the configured serial port
+
+    :param str port: The serial port to use, eg: /dev/ttyUSB0
+    :param int baud: The baud rate to use, eg: 115200
+    :param int timeout: eg: 60
+    :param int parity: eg: serial.PARITY_NONE
+    :param int stop_bits: eg: serial.STOPBITS_ONE
+    :param radio_class: The implementation to use, eg: xbee.zigbee.ZigBee
+    :type radio_class: xbee.base.XBeeBase
+    :param packet_filter: A function that takes one parameter, the parsed radio packet, and returns a bool specifying
+       weather or not to keep the packet.
+    :type packet_filter: lambda a: True
+    :param bool escaped: The radio is in API mode 2
+    """
     def __init__(self, port, baud,
                  timeout=60,
                  parity=serial.PARITY_NONE,
