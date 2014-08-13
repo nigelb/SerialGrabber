525dc6b829e1815f67efdd80cd87b5bb289273e7
NigelB <nigel.blair@gmail.com>
2014-08-13 22:32:48 +1000

    Converts Archive functions into Classes

--- serial_grabber/archive.py
+++ serial_grabber/archive.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 # SerialGrabber reads data from a serial port and processes it with the
 # configured processor.
 # Copyright (C) 2012  NigelB
@@ -18,83 +18,123 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 import SerialGrabber_Paths
-from datetime import datetime
 import logging
 import os, tarfile
 import shutil
+from serial_grabber.util import get_millis
 
-logger = logging.getLogger("Archive")
 
-archive = {}
+class BaseArchive:
+    logger = logging.getLogger("Archive")
 
-def tar_open_archive(depth=0, name="archive"):
-    """
-    The archived transactions are stored in a tar file
-    """
-    if depth == 2:
-        return None
-    global archive
-    if name in archive:
-        return archive[name]
-    if not os.path.exists(SerialGrabber_Paths.archive_dir):
-        os.makedirs(SerialGrabber_Paths.archive_dir)
-    archive_path = os.path.join(SerialGrabber_Paths.archive_dir, "%s.tar"%name)
-    archive_existed = os.path.exists(archive_path)
-    try:
-        archive[name] = tarfile.open(archive_path,"a")
-    except:
-        if archive_existed:
-            n = datetime.now()
-            while os.path.exists(os.path.join(SerialGrabber_Paths.archive_dir, "%s-%s.tar"%(name, n.strftime("%Y_%m_%d_%H_%M_%S")))):
-                n = datetime.now()
-            old_archive_path = os.path.join(SerialGrabber_Paths.archive_dir, ("%s-%s.tar"%(name, n.strftime("%Y_%m_%d_%H_%M_%S"))))
-            logger.error("Could not open archive.tar, moving to %s and starting new archive."%old_archive_path)
-            shutil.move(archive_path, old_archive_path)
-            return tar_open_archive(depth=depth+1)
-    return archive[name]
+    def __init__(self, archive_dir):
+        self.archive_dir = archive_dir
+        self.archives = {}
 
-def tar_close_cache():
-    global archive
-    for name in archive:
-        archive[name].close()
-    logger.warn("Closed cache.")
+    def open(self, depth=0, name="archive"):
+        raise Exception("Not Implemented")
 
-class FileArchive:
+    def archive(self, to_archive, name="archive"):
+        raise Exception("Not Implemented")
 
-    def __init__(self, archive_name):
-        self.archive_name = archive_name
+    def close(self):
+        raise Exception("Not Implemented")
 
-    def add(self, name, arcname=None):
-        with open(self.archive_name, "a") as out:
-            with open(name, "rb") as _in:
-                out.write(_in.read())
-                out.write("\n")
 
-    def close(self):pass
+# class TarArchive(BaseArchive):
+# def __init__(self, archive_dir):
+#         BaseArchive.__init__(self, archive_dir)
+#
+#     def open(self, depth=0, name="archive"):
+#         """
+#         The archived transactions are stored in a tar file
+#         """
+#         if depth == 2:
+#             return None
+#         global archive
+#         if name in archive:
+#             return archive[name]
+#         if not os.path.exists(SerialGrabber_Paths.archive_dir):
+#             os.makedirs(SerialGrabber_Paths.archive_dir)
+#         archive_path = os.path.join(SerialGrabber_Paths.archive_dir, "%s.tar" % name)
+#         archive_existed = os.path.exists(archive_path)
+#         try:
+#             archive[name] = tarfile.open(archive_path, "a")
+#         except:
+#             if archive_existed:
+#                 n = datetime.now()
+#                 while os.path.exists(os.path.join(SerialGrabber_Paths.archive_dir,
+#                                                   "%s-%s.tar" % (name, n.strftime("%Y_%m_%d_%H_%M_%S")))):
+#                     n = datetime.now()
+#                 old_archive_path = os.path.join(SerialGrabber_Paths.archive_dir,
+#                                                 ("%s-%s.tar" % (name, n.strftime("%Y_%m_%d_%H_%M_%S"))))
+#                 self.logger.error(
+#                     "Could not open archive.tar, moving to %s and starting new archive." % old_archive_path)
+#                 shutil.move(archive_path, old_archive_path)
+#                 return self.tar_open_archive(depth=depth + 1)
+#         return archive[name]
+#
+#     def close(self):
+#         for name in self.archives:
+#             archive[name].close()
+#         self.logger.warn("Closed cache.")
 
-def file_open_archive(depth=0, name="archive"):
-    global archive
-    if name in archive:
-        return archive[name]
-    if not os.path.exists(SerialGrabber_Paths.archive_dir):
-        os.makedirs(SerialGrabber_Paths.archive_dir)
-    archive_path = os.path.join(SerialGrabber_Paths.archive_dir, "%s.archive"%name)
-    archive[name] = FileArchive(archive_path)
-    return archive[name]
 
-def file_close_cache():
-    global archive
-    for name in archive:
-        archive[name].close()
-        del archive[name]
-    logger.warn("Closed cache.")
+class FileSystemArchive(BaseArchive):
+    def __init__(self, archive_dir):
+        BaseArchive.__init__(self, archive_dir)
+        if not os.path.exists(archive_dir):
+            os.makedirs(archive_dir)
 
-class NoArchive:
+    def archive(self, to_archive, name="archive"):
+        try:
+            shutil.move(to_archive, SerialGrabber_Paths.archive_dir)
+            return True
+        except Exception, e:
+            self.logger.error("Error archiving file: %s: %s" % (to_archive, e.message))
+        return False
 
-    def add(self, name, arcname=None):pass
-    def close(self):pass
+    def close(self):
+        pass
 
-def dump_open_archive(depth=0, name="archive"):
-    return NoArchive()
 
-def dump_close_cache(): pass
+class DumpArchive(BaseArchive):
+    def close(self):
+        pass
+
+    def archive(self, to_archive, name="archive"):
+        os.remove(to_archive)
+
+    def open(self, depth=0, name="archive"):
+        pass
+
+
+class JSONLineCodec(BaseArchive):
+    def __init__(self, archive_dir, filename_roller=None):
+        BaseArchive.__init__(self, archive_dir)
+        self.filename_roller = filename_roller
+
+    def close(self):
+        for _archive in self.archives:
+            try:
+                self.archives[_archive].close()
+            except Exception, e:
+                self.logger.error("Error closing archive: %s: %s" % (_archive, e.message))
+        self.archives = {}
+
+    def archive(self, to_archive, name="archive"):
+        try:
+            with open(os.path.join(self.archive_dir, self.get_name(name)), "ab") as _output:
+                with open(to_archive, "rb") as _input:
+                    _output.write(_input.read())
+                    _output.write("\n")
+            os.remove(to_archive)
+            return True
+        except Exception, e:
+            self.logger.error("Error archiving file: %s: %s" % (to_archive, e.message))
+        return False
+
+    def get_name(self, name="archive"):
+        if self.filename_roller is not None:
+            return self.filename_roller.calculate_output_name("%s_{ts}.json"%name, get_millis())
+        return "%s.json" % name
