d8cf6dada2ebfe48ea3fa4f85edd12c5cb97f7f8
NigelB <nigel.blair@gmail.com>
2014-11-25 17:29:22 +1000

    Add requests require

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(name='SerialGrabber',
     author_email='nigel.blair@gmail.com',
     packages=find_packages(),
     zip_safe=False,
-    install_requires=["pyserial"],
+    install_requires=["pyserial", "requests"],
     extras_require = {
         'XBee': ["xbee"]
     },
