1e8dcb381555a278dafa59f1e7b27085a6fab765
NigelB <nigel.blair@gmail.com>
2016-12-08 15:34:48 +1000

    Update fake program to function over a serial connection as well as a TCP connection

--- serial_grabber/fake/__init__.py
+++ serial_grabber/fake/__init__.py
@@ -3,7 +3,11 @@
 import argparse
 import sys
 import logging
-from serial_grabber.connections import TcpClient
+
+import serial
+
+from serial_grabber.connections import TcpClient, SerialPort
+
 try:
     from serial_grabber.fake.fakexbee import FakeXbee
     xbee_support = True
@@ -19,23 +23,42 @@ def setup_logging():
 
 def main():
     parser = argparse.ArgumentParser("Fake XBee module")
-    parser.add_argument('sg_address', help="Serial grabber address")
-    parser.add_argument('sg_port', type=int, help="Serial grabber port")
-    parser.add_argument('type', help="Type of fake",
-                        choices=['xbee', 'serial'])
+    subparser = parser.add_subparsers(description="Transport Methods", help="sub-command help.")
+    parser.add_argument('protocol', help="Protocol to speak over the transport",
+                         choices=['xbee', 'serial'])
+
+    serial_p = subparser.add_parser('tty', help="Use a serial port for the transport.")
+    serial_p.add_argument('port', help="Serial port to use")
+    serial_p.add_argument('baud', type=int, help="Baud rate to use")
+    serial_p.add_argument("--parity",metavar="<parity>", dest="parity", action="store", default=serial.PARITY_NONE, help="The parity for the XBee Module, default: PARITY_NONE. Values are PARITY_NONE, PARITY_EVEN, PARITY_ODD, PARITY_MARK, PARITY_SPACE = 'N', 'E', 'O', 'M', 'S'")
+    serial_p.add_argument("--stop-bits",metavar="<stop_bits>", dest="stop_bits", action="store", default=1, type=int, help="The stop bits for the XBee Module, default: 1")
+    serial_p.add_argument("--timeout",metavar="<timeout>", dest="timeout", action="store", default=1, type=int, help="The timeout for the XBee Module, default: 1")
+    serial_p.set_defaults(stream="serial")
+
+
+
+    tcp_p = subparser.add_parser('tcp', help="Use a TCP stream for the transport.")
+    tcp_p.add_argument('sg_address', help="Serial grabber address")
+    tcp_p.add_argument('sg_port', type=int, help="Serial grabber port")
+    tcp_p.set_defaults(stream="tcp")
+
 
     args = parser.parse_args()
-
+    print args
     setup_logging()
 
-    con = TcpClient(args.sg_address, args.sg_port)
+    if args.stream == "tcp":
+        con = TcpClient(args.sg_address, args.sg_port)
+    elif args.stream == "serial":
+        con = SerialPort(args.port, args.baud, args.timeout, args.parity, args.stop_bits)
 
-    if args.type == 'xbee':
+
+    if args.protocol == 'xbee':
         if xbee_support:
             FakeXbee(con).run()
         else:
             print "XBee support not available"
-    elif args.type == 'serial':
+    elif args.protocol == 'serial':
         FakeSerial(con).run()
 
 if __name__ == '__main__':
