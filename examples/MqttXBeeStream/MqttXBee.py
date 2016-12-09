07b1b709cbade2cef32230521aaedd39c157f227
NigelB <nigel.blair@gmail.com>
2016-12-09 17:11:52 +1000

    Add logging to Message Verifier

--- examples/MqttXBeeStream/MqttXBee.py
+++ examples/MqttXBeeStream/MqttXBee.py
@@ -1,16 +1,19 @@
+import logging
+
 from serial_grabber.reader.Xbee import MessageVerifier
 
 
 class XBeeMessageVerifier(MessageVerifier):
+    logger = logging.getLogger("MessageVerifier")
     def verify_message(self, transaction):
         try:
             data = transaction.split("\n")
-            print len("\n".join(data[1:-2])), int(data[-2])
-
             if int(data[-2]) == len("\n".join(data[1:-2])):
                 return True, "OK"
             else:
+                self.logger.error("Reported length: %s, Actual length: %s"%(int(data[-2]), len("\n".join(data[1:-2]))))
                 raise ValueError()
         except ValueError, e:
+            self.logger.error("Could not convert %s to an integer."%data[-2])
             return False, "NA"
 
