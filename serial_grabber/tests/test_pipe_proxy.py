84d18dba7e483100840e32223deb130e490f38f3
NigelB <nigel.blair@gmail.com>
2016-12-13 14:49:01 +1000

    Add PipeProxy tests

---
+++ serial_grabber/tests/test_pipe_proxy.py
@@ -0,0 +1,41 @@
+import unittest
+
+from multiprocessing import Pipe
+
+from serial_grabber.pipe_proxy import expose_object, PipeProxy, RemoteException
+
+
+class TestClass:
+
+    def true(self):
+        return True
+
+    def false(self):
+        return False
+
+    def exception(self):
+        raise Exception()
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_true(self):
+        left, right = Pipe()
+        expose_object(left, TestClass())
+        test = PipeProxy(right)
+        self.assertEqual(test.false(), False)
+        test.__close__()
+
+    def test_exception(self):
+        left, right = Pipe()
+        expose_object(left, TestClass())
+        test = PipeProxy(right)
+        try:
+            test.exception()
+        except RemoteException as re:
+            self.assertTrue(re)
+        test.__close__()
+
+
+if __name__ == '__main__':
+    unittest.main()
