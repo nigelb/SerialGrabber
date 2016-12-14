e4efd51be1d5e971b4d7e306ce6e8715f32dbddb
NigelB <nigel.blair@gmail.com>
2016-12-14 10:20:14 +1000

    Add process id to log messages

--- serial_grabber/SerialGrabber.py
+++ serial_grabber/SerialGrabber.py
@@ -50,9 +50,9 @@ def main():
             os.makedirs(SerialGrabber_Paths.archive_dir)
 
     #Setup up the logging
-    FORMAT = '%(asctime)-15s %(levelname)-7s %(name)s %(filename)s:%(funcName)s:%(lineno)d - %(message)s'
+    FORMAT = '%(asctime)-15s %(levelname)-7s %(process)-6d %(name)s %(filename)s:%(funcName)s:%(lineno)d - %(message)s'
     if 'PYTHON_DEV_ENVIRONMENT' in os.environ and os.environ['PYTHON_DEV_ENVIRONMENT'] == '1':
-        FORMAT = '%(asctime)-15s %(levelname)-7s %(name)s File "%(pathname)s", line %(lineno)d, in %(funcName)s - %(message)s'
+        FORMAT = '%(asctime)-15s %(levelname)-7s %(process)-6d %(name)s File "%(pathname)s", line %(lineno)d, in %(funcName)s - %(message)s'
     log_file =  os.path.join(SerialGrabber_Paths.data_logger_dir, "datalogger.log")
 
 #    logging.basicConfig(format=FORMAT,level=logging.INFO, filename=log_file)
